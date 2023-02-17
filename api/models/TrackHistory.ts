43308c453110282d8305ba9fd0fb1af07dc165bd
Aidai Dzhunushalieva <aidaimond5@gmail.com>
2023-02-17 13:50:04 +0600

    Created new Model TrackHistory.

---
+++ api/models/TrackHistory.ts
@@ -0,0 +1,34 @@
+import mongoose, {Types} from "mongoose";
+import User from "./User";
+import {Track} from "./Track";
+
+const Schema = mongoose.Schema;
+
+const TrackHistorySchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+    validate: {
+      validator: async (value: Types.ObjectId) => User.findById(value),
+      message: 'Artist does not exist',
+    }
+  },
+  track: {
+    type: Schema.Types.ObjectId,
+    ref: 'Track',
+    required: true,
+    validate: {
+      validator: async (value: Types.ObjectId) => Track.findById(value),
+      message: 'Artist does not exist',
+    }
+  },
+  datetime: {
+    type: String,
+    required: true,
+  }
+});
+
+const TrackHistory = mongoose.model('TrackHistory', TrackHistorySchema);
+
+export default TrackHistory;
